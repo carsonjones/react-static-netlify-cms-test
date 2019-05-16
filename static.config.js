c51bf0759ad2c3363d2b061785ee82272e83d95e
Rob <robpeterman573@gmail.com>
2019-05-15 21:44:10 -0400

    netlify cms integration

--- static.config.js
+++ static.config.js
@@ -1,7 +1,26 @@
+import React from 'react'
 import path from 'path'
 import axios from 'axios'
 
 export default {
+  Document: ({
+    Html,
+    Head,
+    Body,
+    children,
+    state: { siteData, renderMeta },
+  }) => (
+    <Html lang="en-US">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+      </Head>
+      <Body>
+        {children}
+      </Body>
+    </Html>
+  ),
   getRoutes: async () => {
     const { data: posts } = await axios.get(
       'https://jsonplaceholder.typicode.com/posts'
